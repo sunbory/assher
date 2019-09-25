98fcdf435704efae23672c0ddd55a3078a1b4610
Oleg Marin <wwolfovich@gmail.com>
2019-09-25 13:20:00 +0300

    Fixed setup.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as rf:
+    long_description = rf.read()
+    
 setup(
     name="assher",
     version="0.7",
     author="Oleg Marin",
     author_email="wWolfovich@gmail.com",
     description="Async SSH/SFTP massive runner",
-    long_description=open("README.md", "r").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wWolfovich/aioqs",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=[
         "asyncssh>=1.12.0",
         "aioqs>=0.5.5",
-        ]
+        ],
     keywords="AIO async SSH SFTP aioqs",
     classifiers=[
         "Programming Language :: Python :: 3",
